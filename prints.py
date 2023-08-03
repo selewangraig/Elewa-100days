898b1f3499db9ce7f2db18842eefb86c495a0868
Graig Selewan <gselewan@gmail.com>
2023-08-03 21:27:35 +0300

    first commit

---
+++ prints.py
@@ -0,0 +1,4 @@
+# Writing my first python program
+print("My name is Graig")
+print("I'm 22 years old")
+print("During this class, I expect to learn python basics and build on it further as we progress.")
---
+++ variables.py
@@ -0,0 +1,21 @@
+# Understanding Variables in Python
+# first representation 'things i love'
+cars = "I love German cars; Porsche"
+food = "I really eat anything, bora sio matumbo fr"
+
+print(cars)
+print(food)
+
+# second representation 'why  i want to learn coding'
+reason_1 = "I want to learn programming as it will help me to jumpstart my SWE career"
+reason_2 = "Coding is fun and i love it"
+
+print(reason_1)
+print(reason_2)
+
+# third representation 'what motivates me'
+motivation_1 = "There's so many problems in the world and I do believe that tech has answers"
+motivation_2 = "Of course, money is a motivation"
+
+print(motivation_1)
+print(motivation_2)
