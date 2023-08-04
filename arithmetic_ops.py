88f36030e50fc5aa2fdd35b1c702208f477c8470
Graig Selewan <gselewan@gmail.com>
2023-08-04 21:42:14 +0300

    Arithmetic Operations

---
+++ arithmetic_ops.py
@@ -0,0 +1,25 @@
+# Basic arithmetic operations in python
+
+a = 10
+b = 4
+
+# Addition
+print(f"This will give the addition of {a} and {b} = {a + b}")
+
+# Subtraction
+print(f"This will give the subtraction of {a} and {b} = {a - b}")
+
+# Multiplication
+print(f"This will give the multiplication of {a} and {b} = {a * b}")
+
+# Division
+print(f"This will give the division of {a} by {b} = {a / b}")
+
+# Modulus
+print(f"This will return the reminder of {a} divided by {b} = {a % b}")
+
+# Division floor
+print(f"This will give the whole number when dividing {a} and {b} = {a // b}")
+
+# Exponent / power
+print(f"This will give {a} power {b} = {a ** b}")
