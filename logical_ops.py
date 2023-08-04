efae9fa5ffa939862723b7cbc8635a94c09085d1
Graig Selewan <gselewan@gmail.com>
2023-08-04 22:24:08 +0300

    first commit

---
+++ logical_ops.py
@@ -0,0 +1,13 @@
+# Logical Operations in Python
+
+a = 10
+b = 4
+
+# and - both conditions must be true
+print(f"Is {a} greater than 5 and {b} less than 10? {a > 5 and b < 10}")
+
+# or - either condition must be true
+print(f"Is {a} greater than 5 or {b} greater than 10? {a > 5 or b > 10}")
+
+# not - reverses the condition
+print(f"Is {a} greater than 5? {not a > 5}")
