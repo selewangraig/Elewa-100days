e13343fcd22edc17dd6f43e2b8be07563704c81b
Graig Selewan <gselewan@gmail.com>
2023-08-09 00:30:34 +0300

    Demonstrated Logical Statements.

--- logical_ops.py
+++ logical_ops.py
@@ -11,3 +11,25 @@ print(f"Is {a} greater than 5 or {b} greater than 10? {a > 5 or b > 10}")
 
 # not - reverses the condition
 print(f"Is {a} greater than 5? {not a > 5}")
+
+
+#Demonstration of using logical operations
+p = True
+q = False
+r = True
+
+#task_1
+a = p or q #True
+aa = a and r #True
+b = p and r #True
+c = q and r #False
+
+ans_1 = aa or b or c #True
+#print(ans_1)
+
+#task_2
+d = not(p and q) #True
+e = q and (not r) #False
+
+ans_2 = d or e #True
+#print(ans_2)
