d3b8a0b19e97e1dc0d442129b148894157de5bc9
Graig Selewan <gselewan@gmail.com>
2023-08-10 09:34:09 +0300

    first commit

---
+++ bits_comparison_task_01.py
@@ -0,0 +1,39 @@
+#Question 1 (7 + 5 >= 10)
+a = 7 + 5 >= 10 #True
+
+print(f"Answer to question 1: {a}")
+
+#Question 2 ("Anii" > "anii")
+b = "Anii" > "anii" #False
+
+print(f"Answer to question 2: {b}")
+
+#Question 3 (25 % 3 == 2)
+c = 25 % 3 == 2 #True
+
+print(f"Answer to question 3: {c}")
+
+#Question 4 (23 // 7 >= 9)
+d = 23 // 7 >= 9 #False
+
+print(f"Answer to question 4: {d}")
+
+#Question 5 (7 + 3 % 2 != True)
+e = 7 + 3 % 2 != True #False
+
+print(f"Answer to question 5: {e}")
+
+#Question 6 (False == False)
+f = False == False #True
+
+print(f"Answer to question 6: {f}")
+
+#Question 7 (True != False)
+g = True != False #True
+
+print(f"Answer to question 7: {g}")
+
+#Question 8 (29 == 89 % 30)
+h = 29 == 89 % 30 #True
+
+print(f"Answer to question 8: {h}")
